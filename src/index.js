b1472248fa57575b73f2453e9ceb1120de9bcbdb
Vyacheslav <dikey677@gmail.com>
2022-04-06 14:17:06 +0300

    update from React 17 to React 18

---
+++ src/index.js
@@ -0,0 +1,25 @@
+import React from "react";
+import App from "./components/App/App";
+import "./index.css";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+
+// React 18
+
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
+
+root.render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
+
+// React 17
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById("root")
+// );
